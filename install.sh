ec7e0c54d7849d6d8fb662c0c847e8304a6c62ba
546220777 <44859578+546220777@users.noreply.github.com>
2022-07-15 17:05:05 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,6 +9,28 @@ else
 fi
 echo -e "\n\t\t\t你的青龙版本为:${QL_BRANCH}\n"
 
+install_depend(){
+
+    echo -e "\n1.开始安装所需依赖\n"
+    # 包依赖
+    apk add zlib zlib-dev libjpeg-turbo libjpeg-turbo-dev gcc python3-dev libffi-dev musl-dev linux-headers
+    # 模块依赖
+    pip3 install qrcode==7.3.1 Telethon==1.24.0 requests==2.27.1 Pillow==9.0.0 python-socks==1.2.4 async_timeout==4.0.2 prettytable==3.0.0
+
+    if [ -f ${QLMainPath}/config/extra.sh ];then
+        if [ `grep "#机器人所需依赖" ${QLMainPath}/config/extra.sh` ];then
+            echo "已设置重启青龙自动启动机器人"
+        else
+            echo -e "解决重启青龙后，jbot失效问题~"
+            echo -e "#机器人所需依赖" >>${QLMainPath}/config/extra.sh
+            echo "apk add zlib zlib-dev libjpeg-turbo libjpeg-turbo-dev gcc python3-dev libffi-dev musl-dev linux-headers" >>${QLMainPath}/config/extra.sh
+            echo "pip3 install qrcode==7.3.1 Telethon==1.24.0 requests==2.27.1 Pillow==9.0.0 python-socks==1.2.4 async_timeout==4.0.2 prettytable==3.0.0" >>${QLMainPath}/config/extra.sh
+            echo "cd ${QLMainPath}/jbot  && pm2 start ecosystem.config.js" >>${QLMainPath}/config/extra.sh
+            echo "cd ${QLMainPath}/ && pm2 start jbot" >>${QLMainPath}/config/extra.sh
+        fi
+    fi
+
+}
 
 gitPull(){
     echo -e "\n2.开始拉取所需代码\n"
@@ -17,7 +39,7 @@ gitPull(){
     else
         rm -rf ${QLMainPath}/jbot/*
     fi
-    cd ${QLMainPath}/repo && rm -rf gd && git clone ${daili}https://github.com/curtinlv/gd.git
+    cd ${QLMainPath}/repo && rm -rf gd && git clone $https://github.com/curtinlv/gd.git
     cp -a ${QLMainPath}/repo/gd/* ${QLMainPath}/jbot && cp -a ${QLMainPath}/jbot/conf/* ${QLMainPath}/config && cp -a ${QLMainPath}/jbot/jk_script/* ${QLMainPath}/scripts
     rm -rf ${QLMainPath}/repo/dockerbot
     mkdir ${QLMainPath}/repo/dockerbot && ln -sf ${QLMainPath}/repo/gd ${QLMainPath}/repo/dockerbot/jbot && ln -sf ${QLMainPath}/repo/gd/conf ${QLMainPath}/repo/dockerbot/config
@@ -38,6 +60,7 @@ if [ -f ${QLMainPath}/jbot/user/user.py ];then
     echo -e "rm -rf  ${QLMainPath}/jbot/*  &&   bash  install.sh\n"
     exit 0
 fi
+install_depend
 gitPull
 echo -e "\n*******************\n所需环境已部署完成\n*******************\n"
 echo -e "请前往面板【配置文件】配置tg机器人参数，再启动机器人即可。\n参考本仓库第3-4步: https://github.com/curtinlv/gd/blob/main/README.md "
